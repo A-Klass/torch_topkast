08e749dcd0a9f9c29fd7c3abb7c1070c8d5438a5
SvenLorenz <sven.lorenz@campus.lmu.de>
2021-08-02 14:58:52 +0200

    not working loss function

---
+++ topkast_loss.py
@@ -0,0 +1,24 @@
+# %%
+import torch
+import torch.nn as nn
+
+
+#%%
+class TopKastLoss(nn.Module):
+    def __init__(self, loss, alpha) -> None:
+        super(TopKastLoss, self).__init__()
+        self.loss = loss
+        self.alpha = alpha
+
+
+    def forward(self, y_hat, y, params):
+        params = params.detach().reshape(-1)
+        l = self.loss(y_hat, y)
+        l += self.alpha * torch.linalg.norm(params)
+
+        return l
+# %%
+loss = TopKastLoss(loss = nn.MSELoss, alpha = 0.1)
+# %%
+loss(torch.rand(10), torch.rand(10), torch.rand(1000))
+# %%
