just relized I had the >= wrong in the masking function

topk_linear.py
@@ -80,10 +80,10 @@ class TopkLinear(nn.Module):
         topk_percentage = topk / w.shape.numel()
         if w.is_sparse:
             threshold = np.quantile(w.values().detach(), topk_percentage)
-            mask = np.where(w.values().detach() <= threshold)
+            mask = np.where(w.values().detach() >= threshold)
         else:
             threshold = np.quantile(w.reshape(-1).detach(), topk_percentage)
-            mask = np.where(w.detach() <= threshold)
+            mask = np.where(w.detach() >= threshold)
         return mask
     
     def forward(self, inputs, sparse = True):