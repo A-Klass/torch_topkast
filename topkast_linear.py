d4d048a6b4bc7c5b1341aa7ba7cea3e46518128e
SvenLorenz <sven.m.lorenz@gmail.com>
2021-08-06 14:41:27 +0200

    working loss

---
+++ topkast_linear.py
@@ -0,0 +1,181 @@
+#%% Imports
+
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+
+#%% TopKast training function
+
+class TopKastTraining(torch.autograd.Function):
+    """ 
+    Custom pytorch function to handle changing connections and topkast.
+    ctx = context in which you can save stuff that doesn't need gradients.
+    """
+
+    @staticmethod
+    def forward(ctx, inputs, weights, bias, indices_forward, indices_backward):
+        
+        # Compute sparse weight tensor
+        weights_used = torch.sparse_coo_tensor(
+            indices=indices_forward, 
+            values=weights[indices_forward], 
+            size=weights.shape)
+        
+        # Compute output as weighted sum of inputs plus bias term
+        output = torch.sparse.addmm(
+            bias.unsqueeze(1), 
+            weights_used, 
+            inputs.t()).t()
+        
+        # Store values in saved tensors to access during backward()
+        ctx.save_for_backward(inputs, weights, bias)
+        
+        # Store backward indices in context
+        ctx.indices_backward = indices_backward
+
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+
+        # Get saved tensors
+        inputs, weights, bias = ctx.saved_tensors
+        
+        # Initialize gradients
+        grad_inputs = grad_weights = grad_bias = None
+        
+        # Get backward indices from context
+        indices_backward = ctx.indices_backward
+
+        # Compute grad wrt inputs if necessary
+        if ctx.needs_input_grad[0]:
+            grad_inputs = grad_output.mm(weights)
+        
+        # Compute grad wrt weights if necessary
+        if ctx.needs_input_grad[1]:
+            grad_weights = grad_output.t().mm(inputs)
+            grad_weights = torch.sparse_coo_tensor(
+                indices=indices_backward,
+                values=grad_weights[indices_backward], 
+                size=grad_weights.shape)
+            
+        # Compute grad wrt bias if necessary (and bias is specified)
+        if bias is not None and ctx.needs_input_grad[2]:
+            grad_bias = grad_output.sum(0)
+
+        return grad_inputs, grad_weights, grad_bias, None, None
+
+#%% TopKast linear layer
+
+class TopKastLinear(nn.Module):
+    """"
+    Sparse adaptation of nn.Linear module with topkast.
+    """
+    
+    def __init__(self, in_features: int, out_features: int, topk_forward: int, 
+                 topk_backward: int, bias: bool=True, device=None, 
+                 dtype=None) -> None:
+        
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        super(TopKastLinear, self).__init__()
+        
+        # Perform basic input checks
+        for i in [in_features, out_features, topk_forward, topk_backward]:
+            assert type(i) == int, 'integer input required'
+            assert i > 0, 'inputs must be > 0'
+        assert topk_backward >= topk_forward
+        assert type(bias) == bool
+            
+        # Initialize
+        self.in_features = in_features
+        self.out_features = out_features
+        self.topk_forward = topk_forward
+        self.topk_backward = topk_backward
+        self.weight = nn.Parameter(
+            torch.empty((out_features, in_features), **factory_kwargs))
+        if bias:
+            self.bias = nn.Parameter(
+                torch.empty(out_features, **factory_kwargs))
+        else:
+            self.register_parameter('bias', None)
+        self.D = self.topk_forward / (self.weight.shape.numel())
+        self.reset_parameters()
+        
+    # Define weight initialization (He et al., 2015)
+
+    def reset_parameters(self) -> None:
+        torch.nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = torch.nn.init._calculate_fan_in_and_fan_out(
+                self.weight)
+            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+            torch.nn.init.uniform_(self.bias, -bound, bound)
+        self.indices_forward = self.compute_mask(self.topk_forward)
+        self.indices_backward = self.compute_mask(self.topk_backward)
+        
+    # Define masking operations
+
+    def compute_mask(self, topk):
+        w = self.weight
+        topk_percentage = topk / w.shape.numel()
+        if w.is_sparse:
+            threshold = np.quantile(w.values().detach(), topk_percentage)
+            mask = np.where(w.values().detach() >= threshold)
+        else:
+            threshold = np.quantile(w.reshape(-1).detach(), topk_percentage)
+            mask = np.where(w.detach() >= threshold)
+        return mask
+    
+    # Define forward pass
+    
+    def forward(self, inputs, sparse=True):
+        
+        self.indices_forward = self.compute_mask(self.topk_forward)
+        self.indices_backward = self.compute_mask(self.topk_backward)
+
+        if sparse:
+            if self.training:
+                # Sparse training
+                output = TopKastTraining.apply(
+                    inputs, 
+                    self.weight, 
+                    self.bias, 
+                    self.indices_forward,
+                    self.indices_backward)
+            else:
+                # Sparse forward pass without training
+                with torch.no_grad():
+                    weights = torch.sparse_coo_tensor(
+                        indices=self.indices_forward, 
+                        values=self.weight[self.indices_forward],
+                        size=self.weight.shape)
+                    output = torch.sparse.addmm(
+                        self.bias.unsqueeze(1), 
+                        weights, 
+                        inputs.t()).t()
+        else:
+            # Dense training is not possible, only a dense forward pass for 
+            # prediction
+            with torch.no_grad():
+                output = torch.addmm(
+                    self.bias.unsqueeze(1), 
+                    self.weight, 
+                    inputs.t()).t()
+        
+        return output
+    
+    # Define field to access sparse weights
+    
+    def sparse_weights(self, forward = True):
+        if forward:
+            weights = torch.sparse_coo_tensor(
+                indices=self.indices_forward, 
+                values=self.weight[self.indices_forward],
+                size=self.weight.shape)
+        else:
+            weights = torch.sparse_coo_tensor(
+                indices=self.indices_backward, 
+                values=self.weight[self.indices_backward],
+                size=self.weight.shape)
+        return weights
--- topkast_loss.py
+++ topkast_loss.py
@@ -2,7 +2,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from topk_linear import TopkLinear
+from topkast_linear import TopKastLinear
 
 
 #%%
@@ -10,65 +10,49 @@ class TopKastLoss(nn.Module):
     """Takes a normal torch.nn.loss and then adds the discounted (alpha) L2-norm of all
     active(!) parameters. Goes through all the layers of the net and looks for TopkLinear 
     Layers and only takes the appropriate weights."""
-    def __init__(self, loss, alpha) -> None:
+    def __init__(self, loss) -> None:
         super(TopKastLoss, self).__init__()
         self.loss = loss()
-        self.alpha = alpha
-
-    def compute_masked_params(self, net):
-        # init param list which gets transformed into a 1d Tensor
-        params = []
-        # Go through all the layers
+    
+    def compute_norm_active_set(self, net):
+        """Updates the forward and backward indices in self"""
+        l2_norm = torch.tensor(0.)
         for child in net.children():
             # Go through all the parameters of the layers
             for name in child._parameters.keys():
                 # Only continue if it's weights. Biases get skipped.
                 if name != 'weight': continue
-                param = child._parameters[name].detach()
-                # If it's a TopkLinear layer it gets stopped. This is hard coded. 
+                # If it's a TopKastLinear layer it gets stopped. This is hard coded. 
                 # If we have more Topk-Layers later we will need a better class system here.
-                if isinstance(child, TopkLinear):
-                    # Compute the mask
-                    topk_backward = child.topk_backward
-                    topk_percentage = topk_backward / param.shape.numel()
-                    if param.is_sparse:
-                        threshold = np.quantile(param.values().detach(), topk_percentage)
-                        mask = np.where(param.values().detach() >= threshold)
-                    else:
-                        threshold = np.quantile(param.reshape(-1).detach(), topk_percentage)
-                        mask = np.where(param.detach() >= threshold)
-                    # Apply the mask
-                    params.append(param[mask].reshape(-1))
+                if isinstance(child, TopKastLinear):                    
+                    l2_norm += torch.linalg.norm(child.sparse_weights())
+                    l2_norm += torch.linalg.norm(child.sparse_weights(forward = False)) / child.D
                 else:
-                    params.append(param.reshape(-1))
-    
-        return torch.cat(params)
+                    l2_norm += torch.linalg.norm(child._parameters[name])
+        
+        return l2_norm
     
     def forward(self, y_hat, y, net):
-        # get the Tensor for all active weights in the backward pass.
-        params = self.compute_masked_params(net)
-        l = self.loss(y_hat, y)
-        l += self.alpha * torch.linalg.norm(params)
+        l = self.loss(y_hat, y) 
+        l += self.compute_norm_active_set(net)
 
         return l
-# %% Don't run, minimal example
-# loss = TopKastLoss(loss = nn.MSELoss, alpha = 0.1)
-# loss(torch.rand(10), torch.rand(10), torch.rand(1000))
+
 # %%
-# class net_a_bit_overkill(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.layer_in = TopkLinear(13, 128, 20, 30)
-#         self.activation_1 = nn.ReLU()
-#         self.hidden = TopkLinear(128, 128, 50, 60)
-#         self.activation_2 = nn.ReLU()
-#         self.layer_out = nn.Linear(128, 1)
+class net_a_bit_overkill(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer_in = TopKastLinear(13, 128, 20, 30)
+        self.activation_1 = nn.ReLU()
+        self.hidden = TopKastLinear(128, 128, 50, 60)
+        self.activation_2 = nn.ReLU()
+        self.layer_out = nn.Linear(128, 1)
 
-#     def forward(self, X):
-#         return self.layer_out(self.activation_2(self.hidden(self.activation_1(self.layer_in(X)))))
+    def forward(self, X):
+        return self.layer_out(self.activation_2(self.hidden(self.activation_1(self.layer_in(X)))))
 
-# net = net_a_bit_overkill()
-# # %%
-# loss = TopKastLoss(loss = nn.MSELoss, alpha = 0.1)
-# loss(torch.rand(10), torch.rand(10), net)
+net = net_a_bit_overkill()
+# %%
+loss = TopKastLoss(loss = nn.MSELoss)
+loss(torch.rand(10), torch.rand(10), net)
 # %%
