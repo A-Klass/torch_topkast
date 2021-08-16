489869c9ddedf099b5e3d4162af111b966000cc9
SvenLorenz <sven.m.lorenz@gmail.com>
2021-08-16 18:51:46 +0200

    burn in phase added, but net is still not learning anything

--- testing routine.py
+++ testing routine.py
@@ -46,6 +46,15 @@ def train(net, num_epochs, loss, optimizer, batch_size, boston = datasets.load_b
     best_net = None
 
     for epoch in range(num_epochs):
+        if epoch < 100:
+            for layer in net.children():
+                if isinstance(layer, TopKastLinear):
+                    layer.update_active_param_set()
+        else:
+            if epoch % 100 == 0:
+               for layer in net.children():
+                   if isinstance(layer, TopKastLinear):
+                        layer.update_active_param_set() 
         for X, y in train_dataset:
             X = X.float()
             y = y.float().reshape((-1, 1))
@@ -56,7 +65,7 @@ def train(net, num_epochs, loss, optimizer, batch_size, boston = datasets.load_b
             optimizer.step()
             losses_train[epoch] += loss_epoch / len(y)
             
-        losses_validation[epoch] = loss(net(validation_dataset[:][0].float()), validation_dataset[:][1].float())
+        losses_validation[epoch] = loss(net(validation_dataset[:][0].float(), sparse=False), validation_dataset[:][1].float())
         if (epoch + 1) % 100 == 0:
             print(f'epoch {epoch + 1}, loss {losses_validation[epoch]:f}') 
         
@@ -72,7 +81,7 @@ def train(net, num_epochs, loss, optimizer, batch_size, boston = datasets.load_b
         if epoch - best_epoch > patience:
             break
     
-    test_loss = loss(net(test_dataset[:][0].float()), test_dataset[:][1].float())
+    test_loss = loss(net(test_dataset[:][0].float(), sparse=False), test_dataset[:][1].float())
 
     return best_net, losses_validation[1:(best_epoch + patience)], losses_train[1:(best_epoch + patience)], best_epoch, test_loss
 
@@ -81,29 +90,38 @@ class TopKastNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.layer_in = TopKastLinear(13, 128, 
-                                      p_forward=0.7, p_backward=0.3)
+                                      p_forward=0.5, p_backward=0.1)
         self.activation_1 = nn.ReLU()
         self.hidden = TopKastLinear(128, 128,
-                                    p_forward=0.7, p_backward=0.3)
-        self.activation_2 = nn.ReLU()
-        self.layer_out = TopKastLinear(128, 1,
-                                       p_forward=0.7, p_backward=0.3)
+                                    p_forward=0.5, p_backward=0.1)
+        # self.activation_2 = nn.ReLU()
+        # self.layer_out = TopKastLinear(128, 1,
+        #                                p_forward=0.7, p_backward=0.3)
 
-    def forward(self, X):
-        return self.layer_out(self.activation_2(self.hidden(self.activation_1(self.layer_in(X)))))
+    def forward(self, X, sparse=True):
+        y_1 = self.layer_in(self.activation_1(X), sparse = sparse)
+        # y_2 = self.hidden(self.activation_2(y_1), sparse = sparse)
+        # y_3 = self.layer_out(y_2, sparse = sparse)
+        
+        return self.self.hidden(y_1, sparse=sparse)
 
 #%%
 net = TopKastNet()
 loss = TopKastLoss(loss = nn.MSELoss, net = net)
-optimizer = torch.optim.SparseAdam(net.parameters())
+optimizer = torch.optim.SparseAdam(net.parameters(), lr=0.1)
 
 #%%
 kast_net, val_loss, train_loss, best_epoch, test_loss = train(net = net, 
       num_epochs = 10000, 
       loss = loss,
       optimizer = optimizer, 
-      batch_size = 128,
-      patience = 20)
+      batch_size = 500,
+      patience = 5)
 
 
+# %%
+import matplotlib.pyplot as plt
+plt.plot(range(len(val_loss)), val_loss, color = "red")
+plt.plot(range(len(train_loss)), train_loss, color = "blue")
+plt.show()
 # %%
