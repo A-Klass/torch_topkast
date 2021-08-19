6336a9c243083bc6ed408d9073b22aa2bdbcbba6
A-Klass <andreas.klass@gmx.de>
2021-08-19 15:53:01 +0200

    on our way to our own package and beyond!

---
+++ _version.py
@@ -0,0 +1,2 @@
+# development version of alpha pre-release
+__version__ = "0.1a1.dev1"
---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+# kudos: https://stackoverflow.com/a/16084844
+exec(open('appl_deepl/_version.py').read())
+ 
+setup(
+    name='appl_deepl_topkast',
+    version=__version__,
+    description='Pytorch Implementation for Top-KAST',
+    long_description=readme,
+    author='Lisa Wimmer, Sven Lorenz, Andreas Klaß',
+    # author_email='was soll hier rein?',
+    url='https://github.com/A-Klass/appl_deepl',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
+# upload to PyPI by: 
+# python setup.py register sdist upload
