4b4103e65c946a3c8fa6d8722302ab0e2f886f80
A-Klass <andreas.klass@gmx.de>
2021-08-21 13:09:57 +0200

    aktualisierte gepulled

---
+++ TopKAST/topkast_linear.py
@@ -0,0 +1,157 @@
+#%% Imports
+
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+import torch_sparse
+
+#%% TopKast linear layer
+
+class TopKastLinear(nn.Module):
+    """"
+    Sparse adaptation of nn.Linear module with topkast.
+    """
+    
+    def __init__(self, in_features: int, out_features: int, p_forward: float, 
+                 p_backward: float, bias: bool=True, device=None, 
+                 dtype=None) -> None:
+        
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        super(TopKastLinear, self).__init__()
+        
+        # Perform basic input checks
+        
+        for i in [in_features, out_features]:
+            assert type(i) == int, 'integer input required'
+            assert i > 0, 'inputs must be > 0'
+        for i in [p_forward, p_backward]:
+            assert type(i) == float, 'float input required'
+            assert i > 0, 'inputs must be between 0 and 1'
+            assert i <= 1, 'inputs must be between 0 and 1'
+        assert p_forward > p_backward
+        assert type(bias) == bool
+            
+        # Initialize
+        
+        self.in_features, self.out_features = in_features, out_features
+        self.p_forward, self.p_backward = p_forward, p_backward
+        self.weight = torch.empty((out_features, in_features), **factory_kwargs)
+        
+        if bias:
+            self.bias = nn.Parameter(
+                torch.empty(out_features, **factory_kwargs))
+        else:
+            self.register_parameter('bias', None)
+            
+        self.reset_parameters()
+        self.weight_vector = None
+        self.update_active_param_set()
+        
+    # Define weight initialization (He et al., 2015)
+
+    def reset_parameters(self) -> None:
+        
+        torch.nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        
+        if self.bias is not None:
+            fan_in, _ = torch.nn.init._calculate_fan_in_and_fan_out(
+                self.weight)
+            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+            torch.nn.init.uniform_(self.bias, -bound, bound)
+  
+    # Define masking operations
+
+    @staticmethod
+    def compute_mask(matrix, p):
+                
+        if matrix.is_sparse:
+            threshold = torch.quantile(matrix.values().detach().abs(), p)
+            mask = torch.where(matrix.values().detach().abs() >= threshold)
+        else:
+            threshold = torch.quantile(matrix.reshape(-1).detach().abs(), p)
+            mask = torch.where(matrix.detach().abs() >= threshold)
+            
+        return mask
+    
+    # Compute set difference between forward and backward active sets
+    # Rather lengthy because np.where's ordering by index must be circumvented
+     
+    def compute_justbwd(self):
+        
+        f = torch.zeros_like(self.weight)
+        b = torch.zeros_like(self.weight)
+        
+        f[self.idx_fwd] = 1
+        b[self.idx_bwd] = 1
+        
+        return torch.where(b - f == 1)
+    
+    # Define update step for active set
+    
+    def update_active_param_set(self) -> None:
+        if self.weight_vector is not None:
+            self.weight[self.idx_bwd] = self.weight_vector.detach()
+        
+        self.idx_fwd = self.compute_mask(self.weight, self.p_forward)
+        self.idx_bwd = self.compute_mask(self.weight, self.p_backward)
+        self.idx_justbwd = self.compute_justbwd()
+        
+        self.weight_vector = nn.Parameter(
+            torch.cat((self.weight[self.idx_fwd].detach(),
+                       torch.zeros(len(self.idx_justbwd[0])).detach())))
+        self.indices = (torch.cat((self.idx_fwd[0], self.idx_justbwd[0])), 
+                        torch.cat((self.idx_fwd[1], self.idx_justbwd[1])))
+        
+        self.set_fwd = range(len(self.idx_fwd[0]))
+        # self.set_bwd = self.weight[self.idx_bwd]
+        self.set_justbwd = range(len(self.idx_fwd[0]), len(self.weight_vector))
+        
+    # Define update step for backward weights
+    
+    # def update_backward_weights(self) -> None:
+    #     breakpoint()
+    #     mat_bwd = torch.zeros_like(self.weight)
+    #     mat_bwd[self.idx_justbwd] = torch_sparse.spmm(
+    #         self.idx_justbwd,
+    #         self.weight_vector,
+    #         self.weight.shape[0],
+    #         self.weight.shape[1],
+    #         torch.diag(torch.ones(self.weight.shape[1])))
+    #     self.weight += mat_bwd
+    
+    # Define forward pass
+    
+    def forward(self, inputs, sparse=True):
+        if sparse:
+            if self.training:
+                # Sparse training
+                output = torch_sparse.spmm(
+                    self.indices, 
+                    self.weight_vector, 
+                    self.out_features, 
+                    self.in_features, 
+                    inputs.t()).t()
+                output += self.bias
+            else:
+                # Sparse forward pass without training
+                with torch.no_grad():
+                    output = torch_sparse.spmm(
+                    self.indices, 
+                    self.weight_vector, 
+                    self.out_features, 
+                    self.in_features, 
+                    inputs.t()).t()
+                    output += self.bias
+        else:
+            # Dense training is not possible, only a dense forward pass for 
+            # prediction
+            with torch.no_grad():
+                output = torch.addmm(
+                    self.bias.unsqueeze(1), 
+                    self.weight, 
+                    inputs.t()).t()
+        
+        return output
+        
+# %%
--- TopKAST/topkast_loss.py
+++ TopKAST/topkast_loss.py
@@ -1,24 +1,64 @@
 # %%
+# In order to use relative import such as:
+# from TopKAST.topkast_linear import TopKastLinear, 
+# first add the the package path to the PYTHONPATH.
+import sys
+sys.path.insert(0, "./TopKAST")
+
+try:
+    from topkast_linear import TopKastLinear
+except ImportError:
+    raise SystemExit("not found. check your relative path")
+  
 import torch
 import torch.nn as nn
-
+from topkast_linear import TopKastLinear
 
 #%%
 class TopKastLoss(nn.Module):
-    def __init__(self, loss, alpha) -> None:
+    """
+    Takes a normal torch.nn.loss and then adds the discounted (alpha) 
+    L2-norm of all active(!) parameters. Goes through all the layers of the net 
+    and looks for TopkLinear layers and only takes the appropriate weights.
+    """
+    def __init__(self, loss, net, alpha=1) -> None:
         super(TopKastLoss, self).__init__()
-        self.loss = loss
+        self.loss = loss()
+        self.net = net
+        assert alpha >= 0 and alpha <= 1
         self.alpha = alpha
-
-
-    def forward(self, y_hat, y, params):
-        params = params.detach().reshape(-1)
-        l = self.loss(y_hat, y)
-        l += self.alpha * torch.linalg.norm(params)
+    
+    def compute_norm_active_set(self):
+        """
+        Updates the forward and backward indices in self
+        """
+        
+        penalty = torch.tensor(0.)
+        
+        for child in self.net.children():
+                         
+            # If the loop encounters a TopKastLinear layer, it stops to compute 
+            # the TopKast-specific penalty.
+            # TODO: adjust if further TopKast layers are added (proper class
+            # system)
+            # For a common layer, all weights are L2-penalized.
+                
+            if isinstance(child, TopKastLinear):                    
+                penalty += torch.linalg.norm(child.weight_vector[child.set_fwd])
+                penalty += (torch.linalg.norm(child.weight_vector[child.set_justbwd]) / 
+                            (1 - child.p_forward))
+            else:
+                for name in child._parameters.keys():
+                    if name != 'weight': continue
+                    penalty += torch.linalg.norm(child._parameters[name])
+        
+        return penalty
+    
+    def forward(self, y_hat, y):
+        
+        l = self.loss(y_hat, y) 
+        l += self.alpha * self.compute_norm_active_set()
 
         return l
-# %%
-loss = TopKastLoss(loss = nn.MSELoss, alpha = 0.1)
-# %%
-loss(torch.rand(10), torch.rand(10), torch.rand(1000))
-# %%
+
+#%%
