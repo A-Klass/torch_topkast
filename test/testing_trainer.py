b4cff199d2aab4c8358211c36104208d233a99a5
A-Klass <andreas.klass@gmx.de>
2021-08-23 19:02:30 +0200

    boson not working when trainer calls self.net(X)

--- test/testing_trainer.py
+++ test/testing_trainer.py
@@ -1,6 +1,7 @@
 """
 Testing procedure including the trainer class
 """
+#%%
 import sys
 # import os
 # path_to_TopKAST = os.path.join(os.getcwd(), "TopKAST")
@@ -34,7 +35,7 @@ import numpy as np
 import copy
 import torch.nn as nn
 import matplotlib.pyplot as plt
-
+#%%
 # Setup a small vanilla net to compare against
 class RegularNet(nn.Module):
     def __init__(self, in_features):
@@ -71,58 +72,59 @@ class TopKastNet(nn.Module):
 
 #%% 
 # Test with synthetic data sporting 2 features
-data = synthetic_dataset(1024)
+data_synthetic = synthetic_dataset(1024)
 #%%
-net = TopKastNet(2)
-loss = TopKastLoss(loss=nn.MSELoss, net=net, alpha=0.4)
+net1 = TopKastNet(2) # synthetic data is 2 dimensional
+loss1 = TopKastLoss(loss=nn.MSELoss, net=net1, alpha=0.4)
 
 # Instantiate a TopKast trainer
-trainer = TopKastTrainer(net,
-                         loss,
+trainer = TopKastTrainer(net1,
+                         loss1,
                          num_epochs=50,
                          num_epochs_explore = 2,
                          update_every = 3,
                          batch_size = 5,
                          patience= 50,
-                         data = data)
-    
+                         data = data_synthetic)
 # and call training method
 trainer.train()
+print("finished training RegularNet(2) for synthetic data")
 #%% 
-net = RegularNet(2)
-loss = TopKastLoss(loss=nn.MSELoss, net=net, alpha=0.4)
-trainer = TopKastTrainer(net,
-                         loss,
+net2 = RegularNet(2)
+loss2 = TopKastLoss(loss=nn.MSELoss, net=net2, alpha=0.4)
+trainer = TopKastTrainer(net2,
+                         loss2,
                          num_epochs_explore = 2,
                          update_every = 3,
                          batch_size = 5,
                          patience= 20,
-                         data = data)
+                         data = data_synthetic)
 trainer.train()
-
+print("finished training RegularNet(2) for synthetic data")
 #%% now with boston which has 13 features
-data = boston_dataset()
-#%%
-net = RegularNet(13)
-loss = TopKastLoss(loss=nn.MSELoss, net=net, alpha=0.4)
-trainer = TopKastTrainer(net,
-                         loss,
-                         num_epochs_explore = 2,
-                         update_every = 3,
-                         batch_size = 5,
-                         patience= 20,
-                         data = data)
-trainer.train()
-#%%
-net = TopKastNet(13)
-loss = TopKastLoss(loss=nn.MSELoss, net=net, alpha=0.4)
-trainer = TopKastTrainer(net,
-                         loss,
-                         num_epochs_explore = 2,
-                         update_every = 3,
-                         batch_size = 5,
-                         patience= 20,
-                         data = data)
-trainer.train()
-
-trainer.eval()
+# data_boston = boston_dataset()
+# #%%
+# net3 = RegularNet(13)
+# loss3 = TopKastLoss(loss=nn.MSELoss, net=net3, alpha=0.4)
+# trainer = TopKastTrainer(net3,
+#                          loss3,
+#                          num_epochs_explore = 2,
+#                          update_every = 3,
+#                          batch_size = 5,
+#                          patience= 20,
+#                          data = data_boston)
+# trainer.train()
+# print("finished training RegularNet(13) for boston data")
+# #%%
+# net4 = TopKastNet(13)
+# loss4 = TopKastLoss(loss=nn.MSELoss, net=net4, alpha=0.4)
+# trainer = TopKastTrainer(net4,
+#                          loss4,
+#                          num_epochs_explore = 2,
+#                          update_every = 3,
+#                          batch_size = 5,
+#                          patience= 20,
+#                          data = data_boston)
+# trainer.train()
+# print("finished training TopKastNet(13) for boston data")
+# trainer.eval()
